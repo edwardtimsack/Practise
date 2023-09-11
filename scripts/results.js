66229b7d204f19df3cadb628e62bf39b814d143a
edwardtimsack <edwardtimothysackie@gmail.com>
2023-09-11 15:44:44 +0000

    most_recent

--- scripts/results.js
+++ scripts/results.js
@@ -52,7 +52,7 @@ const lastestScores = [
             score: '2'
         }
     }
-];
-            
+]; 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 
 
